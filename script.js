3bc0696dd70e16a825e2e1c608643e34203074e9
peters313 <ericpeters@Erics-MBP.hsd1.mn.comcast.net>
2015-07-05 08:37:05 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hey this works!");
+
+function mystery() {
+  var secret = 6;
+  function mystery2(multiplier) {
+    multiplier *= 3;
+    return secret * multiplier;
+  }
+  return mystery2;
+}
+
+var hidden = mystery();
+var result = hidden(3);
